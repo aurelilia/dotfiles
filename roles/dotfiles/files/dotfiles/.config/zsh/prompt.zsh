81e80ccc538ac27f43e46f951f2107058e345b12
Ellie Ang <git@angm.xyz>
2022-02-10 18:09:22 +0100

    Fix prompt issues with command time

--- roles/dotfiles/files/dotfiles/.config/zsh/prompt.zsh
+++ roles/dotfiles/files/dotfiles/.config/zsh/prompt.zsh
@@ -7,18 +7,18 @@ function command_time() {
         min=$(($ZSH_COMMAND_TIME/60))
         sec=$(($ZSH_COMMAND_TIME%60))
         if [ "$ZSH_COMMAND_TIME" -le 60 ]; then
-            timer_show="$fg[green]${ZSH_COMMAND_TIME}s"
+            timer_show="%{$fg[green]%}${ZSH_COMMAND_TIME}s"
         elif [ "$ZSH_COMMAND_TIME" -gt 60 ] && [ "$ZSH_COMMAND_TIME" -le 180 ]; then
-            timer_show="$fg[yellow]${min}min${sec}s"
+            timer_show="%{$fg[yellow]%}${min}min${sec}s"
         else
             if [ "$hours" -gt 0 ]; then
                 min=$(($min%60))
-                timer_show="$fg[red]${hours}h${min}min${sec}s"
+                timer_show="%{$fg[red]%}${hours}h${min}min${sec}s"
             else
-                timer_show="$fg[red]${min}min${sec}s"
+                timer_show="%{$fg[red]%}${min}min${sec}s"
             fi
         fi
-        printf "$fg[white]in $timer_show"
+        printf '%b' "%{$fg[white]%}in $timer_show"
         unset ZSH_COMMAND_TIME
     fi
 }
