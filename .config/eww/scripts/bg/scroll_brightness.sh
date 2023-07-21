0d461af3cefe96bf9ecf89bbdebe8e1815342b82
Ellie Leela Ang <git@elia.garden>
2023-07-21 22:47:10 +0200

    fix brightness

--- .config/eww/scripts/bg/scroll_brightness.sh
+++ .config/eww/scripts/bg/scroll_brightness.sh
@@ -2,7 +2,7 @@
 
 BR=$(brightnessctl g)
 if [ "$1" = "up" ]; then
-    brightnessctl s $(($BR + 1))%
+    brightnessctl s $(($BR + 1))
 else
-    brightnessctl s $(($BR - 1))%
+    brightnessctl s $(($BR - 1))
 fi
