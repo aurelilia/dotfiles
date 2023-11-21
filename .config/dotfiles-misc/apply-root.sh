a59dbb9ba305a07e2d077f1a97bd9a32affa6d23
Leela Aurelia Ang. <git@elia.garden>
2023-11-22 00:13:57 +0100

    Update installer stuff

--- .config/dotfiles-misc/apply-root.sh
+++ .config/dotfiles-misc/apply-root.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 # doas
-cp doas.conf /etc/doas.conf
+cp files/doas.conf /etc/doas.conf
 
 # lightdm
 cp -r lightdm/ /etc
 
 # sanoid
-cp sanoid /etc/cron.d/
+cp files/sanoid /etc/cron.d/
 mkdir -p /etc/sanoid
-cp sanoid.conf /etc/sanoid/
+cp files/sanoid.conf /etc/sanoid/
 
 # services
 dinitctl enable cronie
 dinitctl enable lightdm
 
 # keymap
-cp us /usr/share/X11/xkb/symbols/
+cp files/us /usr/local/share/keymap-us
+cp files/keymap.hook /usr/share/libalpm/hooks/
+cp files/copy-keymap /usr/local/bin/
