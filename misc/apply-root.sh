4c9a2128471ddb078ef4a516868946d278b51992
Leela Aurelia <git@elia.garden>
2023-12-05 11:54:37 +0000

    update packages to be installed

--- misc/apply-root.sh
+++ misc/apply-root.sh
@@ -14,5 +14,7 @@ cp files/sanoid.conf /etc/sanoid/
 # keymap. cheating
 cp .config/home-manager/misc/files/us $(fd us /nix/store | rg symbols/us)
 
-# some packages i want
-pacman -S fish opendoas
+# some packages i want / need
+pacman -S alsa-utils catppuccin-gtk-theme-mocha fish flatpak nfs-utils \
+          ntfs-3g opendoas pipewire-alsa pipewire-pulse pipewire wireplumber \
+          xdg-desktop-portal-wlr xf86-video-amdgpu
