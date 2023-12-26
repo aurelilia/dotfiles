bc2e34cb1d02d56a21bd2d6d05ad7e754735ec4d
Leela Aurelia <git@elia.garden>
2023-12-26 19:13:57 +0000

    fix secrets path

--- fleet/modules/borg.nix
+++ fleet/modules/borg.nix
@@ -1,5 +1,5 @@
 { ... }: {
   # Borg secrets
-  age.secrets."borg-repokey".file = ../secrets/borg-repokey.age;
-  age.secrets."borg-ssh-id".file = ../secrets/borg-ssh-id.age;
+  age.secrets."borg-repokey".file = ../../secrets/borg-repokey.age;
+  age.secrets."borg-ssh-id".file = ../../secrets/borg-ssh-id.age;
 }
