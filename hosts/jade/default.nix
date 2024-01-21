6d114a220617a8a36ca4370aeb4e23b2082a0145
Leela Aurelia <git@elia.garden>
2024-01-22 00:30:56 +0100

    jade: move nfs and tv out of docker

--- hosts/jade/default.nix
+++ hosts/jade/default.nix
@@ -1,10 +1,17 @@
 { config, lib, pkgs, ... }: {
-  imports = [ ./backup.nix ./hardware.nix ];
-
-  # Janky DVB card
-  environment.systemPackages = [ pkgs.libreelec-dvb-firmware ];
-  hardware.firmware = [ pkgs.libreelec-dvb-firmware ];
+  imports = [ ./backup.nix ./hardware.nix ./services/tvheadend.nix ];
 
   # Caddy, for now, until it's migrated into a NixOS container
   networking.firewall.allowedTCPPorts = [ 80 443 8448 ];
+
+  # NFS
+  services.nfs.server.enable = true;
+  fileSystems."/run/nfs/media" = {
+    device = "/media";
+    options = [ "bind" ];
+  };
+  services.nfs.server.exports = ''
+    /run/nfs 10.0.0.0/255.0.0.0(rw,async,no_root_squash,fsid=0)
+    /run/nfs/media 10.0.0.0/255.0.0.0(rw,async,no_root_squash,crossmnt)
+  '';
 }
---
+++ hosts/jade/services/tvheadend.nix
@@ -0,0 +1,9 @@
+{ pkgs, lib, ... }: {
+  services.tvheadend.enable = true;
+  users.users.tvheadend.home = lib.mkForce "/persist/data/tvheadend";
+  networking.firewall.allowedTCPPorts = [ 9981 9982 ];
+
+  # Janky DVB card
+  environment.systemPackages = [ pkgs.libreelec-dvb-firmware ];
+  hardware.firmware = [ pkgs.libreelec-dvb-firmware ];
+}
