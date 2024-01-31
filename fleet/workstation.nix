76a564b7ebed633580e60bc82322dd0ef5000c71
Leela Aurelia <git@elia.garden>
2024-01-31 14:11:47 +0100

    Move some user stuff

--- fleet/workstation.nix
+++ fleet/workstation.nix
@@ -24,20 +24,14 @@ in {
   users.users.leela = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "leela" ];
+    group = "leela";
+    extraGroups = [ "wheel" ];
     hashedPassword =
       "$y$j9T$sHyEvuQc0WD/wi5fxThVv.$tSJK5ie2wmohukSp7tOIEwiOmTF/BPe7u2l/c.L0O79";
     openssh.authorizedKeys.keys = (import ../secrets/keys.nix).ssh;
   };
-  users.groups.leela.gid = 1000;
+  users.groups.leela = {};
   home-manager.users.leela = import ../home/workstation.nix;
-
-  # Ethereal, expected to be present on workstations and owned by leela.
-  # Usually also a mountpoint, but we still want permissions to be correct
-  system.activationScripts.makeEthereal = ''
-    mkdir -p /ethereal/cache
-    chown -R 1000:1000 /ethereal
-  '';
 
   # Misc services I want
   services.gvfs.enable = true;
