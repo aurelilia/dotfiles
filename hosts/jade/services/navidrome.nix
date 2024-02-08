1a2e580064f8d733892cabc41ad7ff3db0533f24
Leela Aurelia <git@elia.garden>
2024-02-08 15:13:26 +0100

    navidrome: https://social.elia.garden/@leela/111896070088643866

--- hosts/jade/services/navidrome.nix
+++ hosts/jade/services/navidrome.nix
@@ -8,18 +8,13 @@ in
     openFirewall = true;
     settings = {
       ScanSchedule = "@every 2h";
-      MusicFolder = "/music";
+      MusicFolder = "/media/personal/music";
       LastFM = {
         # TODO lol, secrets in public git repo
         ApiKey = "7215047f29bbb24662ef06f59c3c9fae";
         Secret = "0945b41f371b9f5acf3d3ceb0107b157";
       };
     };
-  };
-  
-  fileSystems."/music" = {
-    device = "/media/personal/music";
-    options = [ "bind" "ro" ];
   };
 
   elia.persist.navidrome.path = "/var/lib/private/navidrome";
