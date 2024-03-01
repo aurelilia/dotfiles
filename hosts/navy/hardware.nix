05af86329ca982829ea4d27e9fd1676d5e6b3986
Leela Aurelia <git@elia.garden>
2024-03-01 15:25:56 +0100

    improved deploy, navy: swap

--- hosts/navy/hardware.nix
+++ hosts/navy/hardware.nix
@@ -38,7 +38,7 @@
     fsType = "ext4";
   };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/@swap/swapfile"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- nixos/library/auto-deploy.nix
+++ nixos/library/auto-deploy.nix
@@ -1,41 +1,71 @@
-{ pkgs, config, ... }:
 {
-  systemd.services.colmena-autodeploy = {
-    description = "Automatic Colmena Deploy";
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.elia.autodeploy;
+in
+{
+  config = lib.mkIf (cfg.local or (cfg.remotes != [ ])) {
+    systemd.services.colmena-autodeploy = {
+      description = "Automatic Colmena Deploy";
 
-    restartIfChanged = false;
-    unitConfig.X-StopOnRemoval = false;
-    serviceConfig.Type = "oneshot";
+      restartIfChanged = false;
+      unitConfig.X-StopOnRemoval = false;
+      serviceConfig.Type = "oneshot";
 
-    environment = config.nix.envVars // {
-      inherit (config.environment.sessionVariables) NIX_PATH;
-      HOME = "/root";
+      environment = config.nix.envVars // {
+        inherit (config.environment.sessionVariables) NIX_PATH;
+        HOME = "/root";
+      };
+
+      path = with pkgs; [
+        coreutils
+        gnutar
+        xz.bin
+        gzip
+        gitMinimal
+        config.nix.package.out
+        config.programs.ssh.package
+        colmena
+      ];
+
+      script = lib.concatStringsSep "\n" (
+        [
+          ''
+            rm -rf /tmp/dotfiles
+            git clone https://git.elia.garden/leela/dotfiles.git /tmp/dotfiles
+            cd /tmp/dotfiles
+            nix flake update
+          ''
+        ]
+        ++ (lib.optional cfg.local "colmena apply-local")
+        ++ (lib.optional (cfg.remotes != [ ]) (
+          "colmena apply --on " + (lib.concatStringsSep "," cfg.remotes)
+        ))
+      );
+
+      startAt = "04:27";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
     };
 
-    path = with pkgs; [
-      coreutils
-      gnutar
-      xz.bin
-      gzip
-      gitMinimal
-      config.nix.package.out
-      config.programs.ssh.package
-      colmena
-    ];
-
-    script = ''
-      rm -rf /tmp/dotfiles
-      git clone https://git.elia.garden/leela/dotfiles.git /tmp/dotfiles
-      cd /tmp/dotfiles
-      nix flake update
-      colmena apply-local
-    '';
-
-    startAt = "04:27";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
+    systemd.timers.colmena-autodeploy.timerConfig.Persistent = true;
+    elia.notify = [ "colmena-autodeploy" ];
   };
 
-  systemd.timers.colmena-autodeploy.timerConfig.Persistent = true;
-  elia.notify = [ "colmena-autodeploy" ];
+  options.elia.autodeploy = {
+    local = lib.mkOption {
+      type = lib.types.bool;
+      description = "Automatically deploy the current configuration locally.";
+      default = true;
+    };
+    remotes = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "Remote hosts to deploy their configuration to.";
+      default = [ ];
+    };
+  };
 }
