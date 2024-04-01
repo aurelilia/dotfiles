fd4e9bb57881fc93e8ab67872a6492c77ad4a088
Leela Aurelia <git@elia.garden>
2024-04-01 13:59:49 +0200

    Try scanner again

--- hosts/hazyboi/default.nix
+++ hosts/hazyboi/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./backup.nix
@@ -7,4 +7,10 @@
   elia.systemType = "workstation";
 
   virtualisation.libvirtd.enable = true;
+
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [ pkgs.epkowa ];
+  users.users.leela.extraGroups = [ "scanner" ];
+  environment.systemPackages = [ pkgs.skanlite ];
+  nixpkgs.config.allowUnfreePredicate = pkg: lib.hasPrefix "iscan" (lib.getName pkg);
 }
