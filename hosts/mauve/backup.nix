de7de45636b550b3bb7e0989ab749d3e5c123fbd
Leela Aurelia <git@elia.garden>
2024-05-08 09:57:09 +0200

    mauve: backup less

--- hosts/mauve/backup.nix
+++ hosts/mauve/backup.nix
@@ -1,13 +1,4 @@
 { ... }:
 {
-  feline = {
-    borg = {
-      persist.time = "17:30";
-      media = {
-        dirs = [ "/home" ];
-        time = "18:00";
-      };
-    };
-    zfs.znap.remotes = [ "haze" ];
-  };
+  feline.borg.persist.time = "17:30";
 }
