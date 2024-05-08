1829df0ad8f7eafd3f02f34e22efa79e046cfbd6
Leela Aurelia <git@elia.garden>
2024-05-08 09:57:04 +0200

    jade: backup less

--- hosts/jade/backup.nix
+++ hosts/jade/backup.nix
@@ -5,22 +5,21 @@
     borg = {
       persist.time = "04:30";
       media = {
-        dirs = [
-          "/containers"
-          "/media"
-        ];
+        dirs = [ "/media" ];
         time = "03:00";
       };
     };
     zfs = {
       receive-datasets = [ "zbackup/zend" ];
-      znap.enable = true;
-      znap.pools = [
-        "zroot"
-        "zdata"
-      ];
-      znap.destinations.jade = {
-        dataset = "zbackup/zend/jade";
+      znap = {
+        enable = true;
+        pools = [
+          "zroot"
+          "zdata"
+        ];
+        destinations.jade = {
+          dataset = "zbackup/zend/jade";
+        };
       };
     };
   };
