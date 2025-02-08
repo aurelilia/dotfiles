be65dbbc2d42ab5c1b32a4ba1512df0f23f8773b
Leela Aurelia <git@elia.garden>
2025-02-08 19:57:38 +0100

    haze: Slim jellyfin

--- hosts/haze/containers/jellyfin.nix
+++ hosts/haze/containers/jellyfin.nix
@@ -17,24 +17,12 @@
   feline.persist."jellyfin".path = "/var/lib/jellyfin";
   feline.caddy.routes = {
     "media.kitten.works".port = 8096;
-    "request.kitten.works" = {
-      host = "jellyseerr.container:5055";
-      mode = "tailnet";
-    };
-    "q.request.kitten.works" = {
-      host = "jellyseerr.container:7979";
-      mode = "tailnet";
-    };
     "s.request.kitten.works" = {
       host = "jellyseerr.container:8989";
       mode = "tailnet";
     };
     "r.request.kitten.works" = {
       host = "jellyseerr.container:7878";
-      mode = "tailnet";
-    };
-    "p.request.kitten.works" = {
-      host = "jellyseerr.container:9696";
       mode = "tailnet";
     };
   };
@@ -58,12 +46,6 @@
     options = [ "net_cls" ];
   };
 
-  age.secrets.mullvad = {
-    file = ../../../secrets/haze/mullvad.age;
-    path = "/persist/data/qbittorrent/mullvad";
-    symlink = false;
-  };
-
   nixpkgs.config.permittedInsecurePackages = [
     "aspnetcore-runtime-6.0.36"
     "aspnetcore-runtime-wrapped-6.0.36"
@@ -84,68 +66,16 @@
       hostPath = "/persist/data/sonarr";
       isReadOnly = false;
     };
-    mounts."/var/lib/private/prowlarr" = {
-      hostPath = "/persist/data/prowlarr";
-      isReadOnly = false;
-    };
-    mounts."/var/lib/private/jellyseerr" = {
-      hostPath = "/persist/data/jellyseerr";
-      isReadOnly = false;
-    };
-    mounts."/qbit" = {
-      hostPath = "/persist/data/qbittorrent";
-      isReadOnly = false;
-    };
-
-    mounts."/etc/mullvad-vpn" = {
-      hostPath = "/persist/data/mullvad/etc";
-      isReadOnly = false;
-    };
-    mounts."/var/cache/mullvad-vpn" = {
-      hostPath = "/persist/data/mullvad/cache";
-      isReadOnly = false;
-    };
 
     ports = [
       # Web UIs
-      { hostPort = 5055; }
       { hostPort = 7878; }
-      { hostPort = 8989; }
-      { hostPort = 7979; }
       { hostPort = 9696; }
     ];
 
     config =
       { config, ... }:
       {
-        services.mullvad-vpn.enable = true;
-        systemd.services."mullvad-daemon".postStart =
-          let
-            mullvad = pkgs.mullvad-vpn;
-          in
-          ''
-            while ! ${mullvad}/bin/mullvad status >/dev/null; do sleep 1; done
-            account="$(cat /qbit/mullvad)"
-
-            # only login if we're not already logged in otherwise we'll get a new device
-            current_account="$(${mullvad}/bin/mullvad account get | grep "account:" | sed 's/.* //')"
-            if [[ "$current_account" != "$account" ]]; then
-              ${mullvad}/bin/mullvad account login "$account"
-            fi
-
-            ${mullvad}/bin/mullvad relay set location se
-            ${mullvad}/bin/mullvad auto-connect set on
-            ${mullvad}/bin/mullvad lan set allow
-            ${mullvad}/bin/mullvad lockdown-mode set on
-            ${mullvad}/bin/mullvad dns set custom 9.9.9.9
-            ${mullvad}/bin/mullvad connect
-          '';
-
-        services.jellyseerr = {
-          enable = true;
-          openFirewall = true;
-        };
-
         services.radarr = {
           enable = true;
           openFirewall = true;
@@ -154,53 +84,6 @@
           enable = true;
           openFirewall = true;
         };
-        services.prowlarr = {
-          enable = true;
-          openFirewall = true;
-        };
-
-        systemd.services.qbittorrent = {
-          description = "qBittorrent-nox service";
-          documentation = [ "man:qbittorrent-nox(1)" ];
-          after = [
-            "network.target"
-            "mullvad-daemon.service"
-          ];
-          wantedBy = [ "multi-user.target" ];
-          bindsTo = [ "mullvad-daemon.service" ];
-
-          serviceConfig = {
-            Type = "simple";
-            User = "qbittorrent";
-            Group = "qbittorrent";
-
-            ExecStartPre =
-              let
-                preStartScript = pkgs.writeScript "qbittorrent-run-prestart" ''
-                  #!${pkgs.bash}/bin/bash
-
-                  # Create data directory if it doesn't exist
-                  if ! test -d "$QBT_PROFILE"; then
-                    echo "Creating initial qBittorrent data directory in: $QBT_PROFILE"
-                    install -d -m 0755 -o "qbittorrent" -g "qbittorrent" "$QBT_PROFILE"
-                  fi
-                '';
-              in
-              "!${preStartScript}";
-            ExecStart = "${pkgs-unstable.qbittorrent-nox}/bin/qbittorrent-nox";
-          };
-
-          environment = {
-            QBT_PROFILE = "/qbit/data";
-            QBT_WEBUI_PORT = "7979";
-          };
-        };
-
-        users.users.qbittorrent = {
-          group = "qbittorrent";
-          uid = 888;
-        };
-        users.groups.qbittorrent.gid = 888;
       };
   };
 }
