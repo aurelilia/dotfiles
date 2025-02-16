d6250ba3cc17145ec4a4780f2d99f269ca92c00f
Leela Aurelia <git@elia.garden>
2025-02-16 02:03:04 +0100

    fix dns

--- dns.nix
+++ dns.nix
@@ -26,10 +26,10 @@ in
           exchange = "navy.elia.garden";
           preference = 10;
         };
-        txt.data = "v=spf1 a:mail.example.com -all";
+        txt.data = "v=spf1 a:navy.elia.garden ~all";
       };
-      "mail._domainkey".txt.data = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCpe+XScTCIGElLMIdBuQRYWC+jLoed5Z7AtbautKAblQec2JfYEjs0WFv1NcTXmHGlB9aH8nEjtf0popsGB0t/ywU6LhclZA5jsc6hc1037kfaXRfI/pGwcOHyuJRNu/RWthgP1Ac/EDf2QYzA3NPYpMJuntE9EpbvEMDQwC+adwIDAQAB";
-      "_dmarc".txt.data = "v=DMARC1; p=none";
+      "mail._domainkey".txt.data = "v=DKIM1;k=rsa;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCpe+XScTCIGElLMIdBuQRYWC+jLoed5Z7AtbautKAblQec2JfYEjs0WFv1NcTXmHGlB9aH8nEjtf0popsGB0t/ywU6LhclZA5jsc6hc1037kfaXRfI/pGwcOHyuJRNu/RWthgP1Ac/EDf2QYzA3NPYpMJuntE9EpbvEMDQwC+adwIDAQAB";
+      "_dmarc".txt.data = "v=DMARC1;p=none";
     };
     "ehir.art" = {
       "" = {
@@ -38,10 +38,10 @@ in
           exchange = "navy.elia.garden";
           preference = 10;
         };
-        txt.data = "v=spf1 a:mail.example.com -all";
+        txt.data = "v=spf1 a:navy.elia.garden ~all";
       };
-      "mail._domainkey".txt.data = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDnn77i9ppRLxtgZ2FkrPYF9uIjR0sf8AXoH5br/+T6fSrT8JwSVqhkb9k/HHv9UrdABFNfNFsYZT1fjkdcr58WvpENnKb1YfjBr2kBsNczrHWK9z23tpmRpJeymshpXrmKBczYT/UKptAy+R7fS+W93AR+5yQsDBZfBcWwgTYYvQIDAQAB";
-      "_dmarc".txt.data = "v=DMARC1; p=none";
+      "mail._domainkey".txt.data = "v=DKIM1;k=rsa;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDnn77i9ppRLxtgZ2FkrPYF9uIjR0sf8AXoH5br/+T6fSrT8JwSVqhkb9k/HHv9UrdABFNfNFsYZT1fjkdcr58WvpENnKb1YfjBr2kBsNczrHWK9z23tpmRpJeymshpXrmKBczYT/UKptAy+R7fS+W93AR+5yQsDBZfBcWwgTYYvQIDAQAB";
+      "_dmarc".txt.data = "v=DMARC1;p=none";
     };
   };
 }
