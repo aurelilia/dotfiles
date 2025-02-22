d9f77997cd9c1fbb4e2df003194efc3c83571f3f
Leela Aurelia <git@elia.garden>
2025-02-22 01:20:33 +0100

    mastodon: eliminate postgres

--- hosts/haze/containers/mastodon.nix
+++ hosts/haze/containers/mastodon.nix
@@ -11,13 +11,6 @@ let
 in
 {
   feline.compose.mastodon.services = {
-    db = {
-      image = "postgres:14-alpine";
-      container_name = "mastodon-postgres";
-      environment = [ "POSTGRES_HOST_AUTH_METHOD=trust" ];
-      shm_size = "256mb";
-      volumes = [ "${path}/postgres:/var/lib/postgresql/data" ];
-    };
     redis = {
       image = "redis:7-alpine";
       container_name = "mastodon-redis";
@@ -28,7 +21,6 @@ in
       container_name = "mastodon-sidekiq";
       command = "bundle exec sidekiq";
       depends_on = [
-        "db"
         "redis"
       ];
       env_file = "${path}/.env.production";
@@ -38,7 +30,6 @@ in
       image = "ghcr.io/glitch-soc/mastodon-streaming:latest";
       command = "node ./streaming";
       depends_on = [
-        "db"
         "redis"
       ];
       env_file = "${path}/.env.production";
@@ -48,7 +39,6 @@ in
       image = "ghcr.io/glitch-soc/mastodon:latest";
       command = "bundle exec puma -C config/puma.rb";
       depends_on = [
-        "db"
         "redis"
       ];
       env_file = "${path}/.env.production";
