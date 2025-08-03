e38a85aa4d03d557d76dc17320aae0604e6ce242
Leela Aurelia <git@elia.garden>
2025-08-03 21:16:24 +0200

    fix wallpaper script

--- home/files/niri-scripts/wallpaper.sh
+++ home/files/niri-scripts/wallpaper.sh
@@ -6,7 +6,7 @@ do
 	for name in $(niri msg -j outputs | jq -r ".[] | .name")
 	do
 		img=$(shuf -e -n1 ~/personal/images/wallpapers/*)
-		swww img -o -t wipe --transition-duration 0.5 $name $img
+		swww img -o $name -t wipe --transition-duration 0.5 $img
 	done
     sleep 600
 done
