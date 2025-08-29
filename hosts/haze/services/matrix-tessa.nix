a8c2e367322535ecd049c159e8bea55540db3ac2
Leela Aurelia <git@elia.garden>
2025-08-30 00:49:34 +0200

    conduit->continuwuity

--- hosts/haze/services/matrix-tessa.nix
+++ hosts/haze/services/matrix-tessa.nix
@@ -1,19 +1,20 @@
-{ ... }:
+{ pkgs-unstable, ... }:
 let
   url = "matrix.tessa.dog";
   port = 51047;
 in
 {
-  services.matrix-conduit = {
+  services.matrix-continuwuity = {
     enable = true;
+    package = pkgs-unstable.matrix-continuwuity;
     settings.global = {
       server_name = "tessa.dog";
-      inherit port;
+      port = [ port ];
       enable_lightning_bolt = false;
       allow_registration = false;
     };
   };
 
-  feline.persist."matrix/tessa".path = "/var/lib/private/matrix-conduit";
+  feline.persist."matrix/tessa".path = "/var/lib/private/continuwuity";
   feline.caddy.routes."${url}".port = port;
 }
